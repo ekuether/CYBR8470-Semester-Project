f923bc61f0975fa363f917bced6a4787ba942e02
Ethen Kuether <ekuether88@gmail.com>
2022-10-31 18:48:50 -0500

    fixing commits

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,15 +10,22 @@ app.get('/', (req, res) => {
     console.log("test1");
 })
 
-app.post("/dungeon", async(req, res) => {
+app.post("/gamelevel", async(req, res) => {
     try {
-        console.log("test")
-        const { LevelName } = req.body;
-        const newLevel = await pool.query("INSERT INTO GameLevel (LevelName) VALUES($1)",
-        [LevelName]
-        );
+        const { ID, StartRoom, LevelDifficulty, LevelName } = req.body;
+        const newLevel = await pool.query("INSERT INTO GameLevel(ID, LevelName, StartRoom, LevelDifficulty) VALUES($1, $2, $3, $4) RETURNING *", [ID, LevelName, StartRoom, LevelDifficulty]);
+        res.json(newLevel.rows[0])
     } catch (err) {
         console.error(err.message);
+    }
+})
+
+app.get("/gamelevel", async(req, res) => {
+    try {
+        const allLevels = await pool.query("SELECT * FROM GameLevel")
+        res.json(allLevels.rows)
+    } catch (err) {
+        console.log(err.message)
     }
 })
 
