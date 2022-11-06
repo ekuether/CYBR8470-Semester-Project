373ceec92d2223563d60e2144d3e28f45d4bab09
Ethen Kuether <ekuether88@gmail.com>
2022-11-06 15:03:45 -0600

    Added routing to all new APIs

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,8 @@ app.use(express.json());
 
 require('./gamelevelHandler/gamelevelList')(app);
 require('./gamelevelHandler/gamelevel')(app);
+require('./gamelevelHandler/gamelevelRooms')(app);
+require('./gamelevelHandler/gamelevelRoomsList')(app);
 
 require('./itemHandler/itemList')(app);
 require('./itemHandler/item')(app);
@@ -17,10 +19,15 @@ require('./obstacleHandler/obstacle')(app);
 
 require('./playerHandler/playerList')(app);
 require('./playerHandler/player')(app);
+require('./playerHandler/playeritems')(app);
+require('./playerHandler/playeritemsList')(app);
 
 require('./roomHandler/roomList')(app);
 require('./roomHandler/room')(app);
-
+require('./roomHandler/roomItems')(app);
+require('./roomHandler/roomItemsList')(app);
+require('./roomHandler/roomObstacles')(app);
+require('./roomHandler/roomObstaclesList')(app);
 
 
 app.listen(3000, function() {
