44eedd2a6d8d8a428eb814882d55ec24e9641452
Kuether, Ethen <Ethen.Kuether@fiserv.com>
2022-12-14 11:29:30 -0600

    added a level select to the frontend

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -11,12 +11,48 @@ import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel'
 import Select from '@mui/material/Select'
 import MenuItem from '@mui/material/MenuItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemText from '@mui/material/ListItemText'
+import ListItem from '@mui/material/ListItem'
+import List from '@mui/material/List'
 
+
+function LevelListItem (props: any) {
+  const {level, onChange} = props
+  return (
+    <ListItem>
+      <ListItemButton onClick={() => onChange(level)}>
+        <ListItemText primary={level} />
+      </ListItemButton>
+    </ListItem>
+  );
+}
+
+function LevelList (props: any) {
+  const {levels, onChange} = props;
+  const ret = []
+
+  for (const l in levels) {
+    ret.push(LevelListItem({'level': levels[l], 'onChange': onChange}))
+  }
+
+  return (
+    <List>
+      {ret}
+    </List>
+  )
+}
 
 export default function Home() {
   const[loginDrawer, changeLoginDrawer] = React.useState(false);
   const[levelDrawer, changeLevelDrawer] = React.useState(false);
   const[option, changeOption] = React.useState('');
+  const[level, changeLevel] = React.useState('');
+
+  const levelChange = (levelid: any) => {
+    console.log(levelid);
+    changeLevel(levelid);
+  }
 
   const handleChangeOption = (event: any) => {
     changeOption(event.target.value);
@@ -26,10 +62,13 @@ export default function Home() {
     <Box sx={{ flexGrow: 1}}>
       <AppBar>
         <Toolbar>
-          <IconButton color='inherit' edge='start' size='large' aria-label='levels' sx={{ mr: 2 }}>
+          <IconButton color='inherit' edge='start' size='large' aria-label='levels' sx={{ mr: 2 }} onClick={() => changeLevelDrawer(!levelDrawer)}>
             <Menu/>
           </IconButton>
           <Drawer anchor={'left'} open={levelDrawer} onClose={() => changeLevelDrawer(!levelDrawer)} >
+            <Box>
+              <LevelList levels={{"Test": "Hello"}} onChange={levelChange} />
+            </Box>
           </Drawer>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Dungeon Creator
